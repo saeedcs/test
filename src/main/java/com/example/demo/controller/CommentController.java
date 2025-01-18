c5f3eee817009efa251d3520aac00b6d9ee0d4c3
Saeed Iqbal <xp.saeed.iqbal@inovalon.com>
2025-01-18 11:35:50 +0500

    added controller classes and configuration files

---
+++ src/main/java/com/example/demo/controller/CommentController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/comments")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @GetMapping
+    public String getAllComments() {
+        return commentService.fetchAllComments();
+    }
+}
---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public String getAllUsers() {
+        return userService.fetchAllUsers();
+    }
+}
